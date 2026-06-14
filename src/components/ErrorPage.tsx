cb4251849212c94502f7038d94ba76a3f105cc76
Dandilla-dev <danilagorshunov@gmail.com>
2026-06-14 22:46:18 +0300

    Error Header

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,6 +1,10 @@
 
 import Text from "./ui/Text.tsx"
+import {SWContext} from "../utils/context.ts";
+import {useContext, useEffect} from "react";
 const ErrorPage = () => {
+    const {changeHero} = useContext(SWContext);
+    useEffect (() => changeHero(''), [changeHero])
     return (
     <Text className={'text-center'}> O-o-ops! Something went wrong.</Text>
 )
